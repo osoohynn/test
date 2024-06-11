03731900fa9ac0a802acca18f4c447f7c8b199cb
su080915 <tbr335@naver.com>
2024-06-11 20:53:51 +0900

    chl

--- 프밍2인수행/이차방정식해2.py
+++ 프밍2인수행/이차방정식해2.py
@@ -1,5 +1,7 @@
 import math 
 def fun_2(a,b,c):
     return (((-b+math.sqrt(b**2-4*a*c))/2*a)*(-1), ((-b-math.sqrt(b**2-4*a*c))/2*a)*(-1))
+    # (x + "이부분")(x + "이부분")
+    # 으로 만듬. 해를 구하려면 여기에 (-1)곱하면 됨.
 
-print(fun_2(1,-2,1))
+print(fun_2(1,4,-12))
